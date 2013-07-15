2a5f2408bb8f2ebf8540953c2fa6ca6a74c7d19b
Niktux <nico.ln@gmail.com>
2013-07-15 18:17:18 +0200

    FIX namespace issue

--- lib/Addendum/Parser/NestedAnnotationMatcher.php
+++ lib/Addendum/Parser/NestedAnnotationMatcher.php
@@ -2,11 +2,13 @@
 
 namespace Addendum\Parser;
 
+use Addendum\AnnotationsBuilder;
+
 class NestedAnnotationMatcher extends AnnotationMatcher
 {
     protected function process($result)
     {
-        $builder = new AnnotationsBuilder;
+        $builder = new AnnotationsBuilder();
         
         return $builder->instantiateAnnotation($result[1], $result[2]);
     }
